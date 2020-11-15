85e99f8e8fa8d797218b44fb3c65e7c95febf812
ColomboRiccardo <riccardocolombopro@gmail.com>
2020-11-15 19:21:09 +0100

    changed url for database

--- controllers/register.js
+++ controllers/register.js
@@ -27,7 +27,7 @@ const handleRegister = (req, res, db, bcrypt, salt) => {
 			})
 			.then(trx.commit)
 			.catch(trx.rollback);
-	}).catch(err => res.status(400).json(err));
+	}).catch(err => res.status(400).json('unable to join'));
 };
 
 module.exports = {
